f3219800d3f99ada9bd6ec58a5c1d9af04f7d046
Danish Khan <iamdanishk555@gmail.com>
2022-06-30 20:01:16 +0545

    fragments and navigation components created

--- app/src/main/java/com/example/newsapiclientapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapiclientapp/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.newsapiclientapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapiclientapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // 1f1157af1c7548daa64ffbb15f12e836
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bnvNews.setupWithNavController(navController)
     }
 }
