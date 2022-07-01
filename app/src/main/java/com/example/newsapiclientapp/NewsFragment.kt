45a74373909ff042464848e1c67a211944643143
Danish Khan <iamdanishk555@gmail.com>
2022-07-01 21:44:52 +0545

    Displaying the news info on a web view

--- app/src/main/java/com/example/newsapiclientapp/NewsFragment.kt
+++ app/src/main/java/com/example/newsapiclientapp/NewsFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AbsListView
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapiclientapp.data.util.Resource
@@ -42,6 +43,12 @@ class NewsFragment : Fragment() {
         fragmentNewsBinding = FragmentNewsBinding.bind(view)
         viewModel = (activity as MainActivity).viewModel
         newsAdapter = (activity as MainActivity).newsAdapter
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("selected_article", it)
+            }
+            findNavController().navigate(R.id.action_newsFragment_to_infoFragment, bundle)
+        }
 
         initRecyclerView()
         viewNewsList()
--- app/src/main/java/com/example/newsapiclientapp/presentation/adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/newsapiclientapp/presentation/adapter/NewsAdapter.kt
@@ -10,28 +10,27 @@ import com.bumptech.glide.Glide
 import com.example.newsapiclientapp.data.model.Article
 import com.example.newsapiclientapp.databinding.NewsListItemBinding
 
-class NewsAdapter:RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
-    private val callback = object : DiffUtil.ItemCallback<Article>(){
+    private val callback = object : DiffUtil.ItemCallback<Article>() {
 
         override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
             return oldItem.url == newItem.url
         }
 
         override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
-           return oldItem == newItem
+            return oldItem == newItem
         }
     }
 
-    val differ = AsyncListDiffer(this,callback)
+    val differ = AsyncListDiffer(this, callback)
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val binding =
-            NewsListItemBinding .inflate(LayoutInflater.from(parent.context),parent,false)
+            NewsListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return NewsViewHolder(binding)
     }
-
 
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
@@ -43,19 +42,30 @@ class NewsAdapter:RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
         return differ.currentList.size
     }
 
-    inner class NewsViewHolder(private val binding:NewsListItemBinding):
-        RecyclerView.ViewHolder(binding.root){
+    inner class NewsViewHolder(private val binding: NewsListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-           fun bind(article: Article){
-               Log.i("MYTAG","came here ${article.title}")
-               binding.tvTitle.text = article.title
-               binding.tvDescription.text = article.description
-               binding.tvPublishedAt.text = article.publishedAt
-               binding.tvSource.text = article.source.name
+        fun bind(article: Article) {
+            Log.i("MYTAG", "came here ${article.title}")
+            binding.tvTitle.text = article.title
+            binding.tvDescription.text = article.description
+            binding.tvPublishedAt.text = article.publishedAt
+            binding.tvSource.text = article.source.name
 
-               Glide.with(binding.ivArticleImage.context).
-               load(article.urlToImage).
-               into(binding.ivArticleImage)
-           }
+            Glide.with(binding.ivArticleImage.context).load(article.urlToImage)
+                .into(binding.ivArticleImage)
+
+            binding.root.setOnClickListener {
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
         }
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
 }
